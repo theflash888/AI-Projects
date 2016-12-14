7139911c56549671d903b445be1f13244ad3c5a5
Mohamad Vedut <theflash888@users.noreply.github.com>
2016-12-14 11:32:26 -0500

    Add files via upload

---
+++ SmartStudentExchange/config/services.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+	| default location for this type of information, allowing packages
+	| to have a conventional place to find your various credentials.
+	|
+	*/
+
+	'mailgun' => [
+		'domain' => 'email.mafam.co',
+		'secret' => 'key-636ba42abaf21207e122d1dcb553ae44',
+	],
+
+	'mandrill' => [
+		'secret' => '',
+	],
+
+	'ses' => [
+		'key' => '',
+		'secret' => '',
+		'region' => 'us-east-1',
+	],
+
+	'stripe' => [
+		'model'  => 'User',
+		'secret' => '',
+	],
+
+];
---
+++ SmartStudentExchange/database/migrations/2015_06_02_011536_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('xchange_id');
+			$table->string('username')->unique();
+			$table->string('fullname');
+			$table->string('email');
+			$table->text('access_token');
+			$table->text('password');
+			$table->string('role');
+			$table->tinyInteger('activated')->default(0);
+			$table->timestamps();
+			//$table->primary('xchange_id');
+			$table->integer('institution_id')->unsigned()->default(1);
+			$table->foreign('institution_id')->references('id')->on('institution')
+				  ->onDelete('restrict')->onUpdate('cascade');
+			$table->string('confirmation_code')->nullable();
+			$table->rememberToken();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('users');
+	}
+
+}
